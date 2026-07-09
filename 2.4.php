240402c95249f2c671310ebdf37005faa8cfed43
vyom2708 <vyompurohit.133430@marwadiuniversity.ac.in>
2026-07-09 08:25:35 +0530

    Add files via upload

---
+++ 2.4.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Vyom Purohit";
+    echo(strlen($name)."<br>");
+    echo(strpos($name,"V")."<br>");
+    echo(str_word_count($name)."<br>");
+    echo(strrev($name)."<br>");
+    echo(strtolower($name)."<br>");
+    echo(strtoupper($name)."<br>");
+?>
